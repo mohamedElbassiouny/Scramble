957203ebb67b21941d5417733ef6967c4b32f445
Mohamed Elbassiouny <apat4e@gmail.com>
2019-08-18 13:36:12 +0200

    init project

---
+++ Scramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scramble
+//
+//  Created by Mohamed Elbassiouny on 8/18/19.
+//  Copyright © 2019 Mohamed Elbassiouny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
